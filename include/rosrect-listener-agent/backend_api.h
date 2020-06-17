8db404b0dd7be5e5acf1763621ec842818c92f3b
swaroophs <swarooph@cognicept.systems>
2020-06-17 09:02:49 +0800

    After interactively testing the post method.

--- include/rosrect-listener-agent/backend_api.h
+++ include/rosrect-listener-agent/backend_api.h
@@ -28,10 +28,13 @@ class BackendApi {
   std::string ecs_api_host;
   std::string ecs_api_endpoint;
   std::string ecs_robot_model;
-  
+  /* Downstream push/post variables below */
+  std::string agent_post_api;
+
   public:
   BackendApi();
   ~BackendApi();
+  pplx::task<void> post_event_log(web::json::value); // A configurable downstream push method
   void push_event_log(std::vector<std::vector<std::string>>); // Create and push single JSON record payload data for downstream consumption
   web::json::value create_event_log(std::vector<std::vector<std::string>>); // Create JSON "multiple record" payload data for downstream consumption
   /* Error classification features in development below */
--- src/backend_api.cpp
+++ src/backend_api.cpp
@@ -1,14 +1,15 @@
 #include <rosrect-listener-agent/backend_api.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
-using namespace ::pplx;                     // PPLX for tasks
-using namespace web::json;                  // JSON features
+using namespace utility;              // Common utilities like string conversions
+using namespace web;                  // Common features like URIs.
+using namespace web::http;            // Common HTTP functionality
+using namespace web::http::client;    // HTTP client features
+using namespace concurrency::streams; // Asynchronous streams
+using namespace ::pplx;               // PPLX for tasks
+using namespace web::json;            // JSON features
 
-BackendApi::BackendApi() {
+BackendApi::BackendApi()
+{
 
   // std::cout << "Creating API instance..." << std::endl;
 
@@ -17,41 +18,43 @@ BackendApi::BackendApi() {
   this->site_id = std::getenv("SITE_CODE");
   this->agent_id = std::getenv("AGENT_ID");
   this->agent_mode = std::getenv("AGENT_MODE");
+  this->agent_post_api = std::getenv("AGENT_POST_API");
 
   // File variables
   std::string run_id;
   bool check_id = ros::param::has("run_id");
   std::string parent_dir = std::getenv("HOME");
-  if(check_id)
+  if (check_id)
   {
     ros::param::get("run_id", run_id);
     parent_dir.append("/.cognicept/agent/logs/" + run_id);
-    std::cout<< "ROS session detected" << std::endl;
+    std::cout << "ROS session detected" << std::endl;
   }
   else
   {
     parent_dir.append("/.cognicept/agent/logs/unittest_logs");
-    std::cout<< "NO ROS session detected" << std::endl;
-  } 
-  
+    std::cout << "NO ROS session detected" << std::endl;
+  }
+
   boost::filesystem::path dir2(parent_dir);
   if (boost::filesystem::exists(dir2))
   {
-    std::cout<< "Agent log directory already exists: "<< parent_dir <<std::endl;
+    std::cout << "Agent log directory already exists: " << parent_dir << std::endl;
   }
   else
   {
-    if(boost::filesystem::create_directories(dir2))
+    if (boost::filesystem::create_directories(dir2))
     {
-      std::cout<< "Agent log directory created: "<< parent_dir <<std::endl;
+      std::cout << "Agent log directory created: " << parent_dir << std::endl;
     }
   }
-  
+
   this->log_name = parent_dir + "/logData";
   this->log_ext = ".json";
   this->log_id = 0;
 
-  if(this->agent_mode == "TEST"){
+  if (this->agent_mode == "TEST")
+  {
     std::cout << "TEST mode is ON. JSON Logs will be saved here: " << parent_dir << std::endl;
   }
 
@@ -62,12 +65,57 @@ BackendApi::BackendApi() {
   this->ecs_robot_model = std::getenv("ECS_ROBOT_MODEL");
 }
 
-BackendApi::~BackendApi() {
+BackendApi::~BackendApi()
+{
 
   // std::cout << "Logged out of API..." << std::endl;
 }
 
-void BackendApi::push_event_log(std::vector<std::vector<std::string>> log){
+pplx::task<void> BackendApi::post_event_log(json::value payload)
+{
+
+  return pplx::create_task([this, payload] {
+           // Create HTTP client configuration
+           http_client_config config;
+           config.set_validate_certificates(false);
+
+           // Create HTTP client
+           http_client client(this->agent_post_api, config);
+
+           // // Write the current JSON value to a stream with the native platform character width
+           // utility::stringstream_t stream;
+           // postData.serialize(stream);
+
+           // // Display the string stream
+           // std::cout << "Post data: " << stream.str() << std::endl;
+
+           // Build request
+           http_request req(methods::POST);
+           // req.headers().add("Authorization", this->headers);
+           req.set_request_uri("/api/agentstream/putRecord");
+           req.set_body(payload);
+
+           // Request ticket creation
+           std::cout << "Pushing downstream..." << std::endl;
+           return client.request(req);
+         })
+      .then([this](http_response response) {
+        // If successful, print ticket details
+        if (response.status_code() == status_codes::OK)
+        {
+          auto body = response.extract_string();
+          std::wcout << "Response: " << body.get().c_str() << std::endl;
+        }
+        // If not, request failed
+        else
+        {
+          std::cout << "Request failed" << std::endl;
+        }
+      });
+}
+
+void BackendApi::push_event_log(std::vector<std::vector<std::string>> log)
+{
   // Create JSON payload and push to kinesis
   auto last_log = log.back();
   int idx = 0;
@@ -82,15 +130,16 @@ void BackendApi::push_event_log(std::vector<std::vector<std::string>> log){
   std::string description = last_log[idx++];
   std::string resolution = last_log[idx++];
   std::string event_id = last_log[idx++];
-  
+
   bool ticketBool = false;
-  if((level == "Error") && ((cflag == "false") || (cflag == "Null"))){
+  if ((level == "8") && ((cflag == "false") || (cflag == "Null")))
+  {
     ticketBool = true;
   }
   else
   {
     ticketBool = false;
-  }  
+  }
 
   // Create JSON object
   json::value payload = json::value::object();
@@ -124,8 +173,9 @@ void BackendApi::push_event_log(std::vector<std::vector<std::string>> log){
   payload[ticketKey] = json::value::boolean(U(ticketBool));
   payload[descKey] = json::value::string(U(description));
   payload[resKey] = json::value::string(U(resolution));
-  
-  if(this->agent_mode == "TEST"){
+
+  if (this->agent_mode == "TEST")
+  {
     // Write the current JSON value to a stream with the native platform character width
     utility::stringstream_t stream;
     payload.serialize(stream);
@@ -133,7 +183,7 @@ void BackendApi::push_event_log(std::vector<std::vector<std::string>> log){
     // Display the string stream
     // std::cout << stream.str() << std::endl;
     std::cout << level << " level event logged with id: " << event_id << std::endl;
-    
+
     // Write to file
     std::ofstream outfile;
     this->log_id++;
@@ -143,11 +193,14 @@ void BackendApi::push_event_log(std::vector<std::vector<std::string>> log){
     outfile << std::setw(4) << stream.str() << std::endl;
     outfile.close();
   }
-  
+
+  // Post downstream
+  this->post_event_log(payload);
 }
 
-json::value BackendApi::create_event_log(std::vector<std::vector<std::string>> log){
-  
+json::value BackendApi::create_event_log(std::vector<std::vector<std::string>> log)
+{
+
   // Create JSON object
   json::value event_log = json::value::array();
 
@@ -164,7 +217,8 @@ json::value BackendApi::create_event_log(std::vector<std::vector<std::string>> l
   utility::string_t srcKey(U("Source"));
 
   // std::cout << "Creating JSON log" << std::endl;
-  for(int queue_id = 0; queue_id < log.size(); queue_id++)  {
+  for (int queue_id = 0; queue_id < log.size(); queue_id++)
+  {
 
     // Get row
     auto current_row = log[queue_id];
@@ -191,83 +245,79 @@ json::value BackendApi::create_event_log(std::vector<std::vector<std::string>> l
     event_log[queue_id][msgKey] = json::value::string(U(message));
     event_log[queue_id][descKey] = json::value::string(U(description));
     event_log[queue_id][resKey] = json::value::string(U(resolution));
-    event_log[queue_id][eidKey] = json::value::string(U(event_id));    
-    event_log[queue_id][qidKey] = json::value::string(U(qidstr));       
-
+    event_log[queue_id][eidKey] = json::value::string(U(event_id));
+    event_log[queue_id][qidKey] = json::value::string(U(qidstr));
   }
 
-  return(event_log);
-
+  return (event_log);
 }
 
 /* Error Classification Features in development below */
 
-pplx::task<void> BackendApi::query_error_classification(std::string msg_text){  
-  
-  return pplx::create_task([this, msg_text]
-  {
-    // Create HTTP client configuration
-    http_client_config config;
-    config.set_validate_certificates(false);
-    
-    // Create HTTP client
-    http_client client(this->ecs_api_host, config);
+pplx::task<void> BackendApi::query_error_classification(std::string msg_text)
+{
 
-    // Build request
-    http_request req(methods::GET);
+  return pplx::create_task([this, msg_text] {
+           // Create HTTP client configuration
+           http_client_config config;
+           config.set_validate_certificates(false);
 
-    // Build request URI.
-    uri_builder builder(this->ecs_api_endpoint);
-    builder.append_query("RobotModel", this->ecs_robot_model);
-    builder.append_query("ErrorText", msg_text);
-    req.set_request_uri(builder.to_string());
-        
-    return client.request(req);
+           // Create HTTP client
+           http_client client(this->ecs_api_host, config);
 
-  })
-  .then([this](http_response response)
-  {
-    
-    // If successful, return JSON query
-    if(response.status_code() == status_codes::OK)
-    {
-      auto body = response.extract_string();
-      std::string body_str = body.get().c_str();
-      this->msg_resp = body_str;
-    }
-    // If not, request failed
-    else
-    {
-      std::cout << "Request failed" << std::endl;
-    }
-  });
+           // Build request
+           http_request req(methods::GET);
+
+           // Build request URI.
+           uri_builder builder(this->ecs_api_endpoint);
+           builder.append_query("RobotModel", this->ecs_robot_model);
+           builder.append_query("ErrorText", msg_text);
+           req.set_request_uri(builder.to_string());
+
+           return client.request(req);
+         })
+      .then([this](http_response response) {
+        // If successful, return JSON query
+        if (response.status_code() == status_codes::OK)
+        {
+          auto body = response.extract_string();
+          std::string body_str = body.get().c_str();
+          this->msg_resp = body_str;
+        }
+        // If not, request failed
+        else
+        {
+          std::cout << "Request failed" << std::endl;
+        }
+      });
 }
 
-json::value BackendApi::check_error_classification(std::string msg_text){
+json::value BackendApi::check_error_classification(std::string msg_text)
+{
 
   this->query_error_classification(msg_text).wait();
   std::string temp_msg = this->msg_resp;
   json::value response = json::value::parse(temp_msg);
   json::value response_data;
-  
-  try{
+
+  try
+  {
     // std::cout << "Trying to get data..." << std::endl;
-    response_data = response.at(U("data")); 
-    
+    response_data = response.at(U("data"));
+
     // // Write the current JSON value to a stream with the native platform character width
     // utility::stringstream_t stream;
     // response_data.serialize(stream);
 
     // Display the string stream
-    // std::cout << "Get data: " << stream.str() << std::endl; 
-    return response_data[0];  
+    // std::cout << "Get data: " << stream.str() << std::endl;
+    return response_data[0];
   }
-  catch(const json::json_exception& e){
+  catch (const json::json_exception &e)
+  {
     // const std::exception& e
     // std::cout << " Can't get data, returning null" << std::endl;
     response_data = json::value::null();
     return response_data;
-  }   
-
+  }
 }
-
