4c299a09c777e83a28b837f14eb52d5c025ea130
Alok Pathak <alok@cognicept.systems>
2020-09-17 17:52:51 +0800

    Trying Docker Approach

--- ci-test.sh
+++ ci-test.sh
@@ -9,6 +9,18 @@ echo "Checking ECS API Val"
 echo $ECS_API
 
 
-pip3 install -r rosrect-ecs-api-server/requirements.txt
-python3 rosrect-ecs-api-server/src/ecs_endpoint.py &
-APP_PID=$!
+# pip3 install -r rosrect-ecs-api-server/requirements.txt
+# python3 rosrect-ecs-api-server/src/ecs_endpoint.py &
+# APP_PID=$!
+
+cd rosrect-ecs-api-server
+docker build -t rosrect_ecs_api_server .
+
+docker run -it \
+--env-file runtime.env \
+-p 8000:8000 \
+--name=ecs_api_server  \
+--net=host \
+--volume="${HOME}/rosrect-ecs-api-server/ecs.db:/root/.cognicept/ecs.db" \
+rosrect_ecs_api_server:latest  \
+ecs_api_server/ecs_endpoint.py
