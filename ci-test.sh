ff56395ad1d8403075f8e2c5e5a5e08395d34f7f
Alok Pathak <alok@cognicept.systems>
2020-09-17 15:21:23 +0800

    Added ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -1,16 +1,6 @@
-#Rewriting the test file
 
-source /opt/ros/melodic/setup.bash
 
-# git clone the folder here 
-
-mkdir -p catkin_test/src
-cd catkin_test/src
-git init 
-git clone https://github.com/cognicept-admin/rosrect-listener-agent.git
-cd rosrect-listener-agent
-git fetch && git checkout travis-ci
-cd ..
+#chmod +x rosrect-ecs-api-server/src/ecs_endpoint.pynohup python3 rosrect-ecs-api-server/src/ecs_endpoint.py &
 
 #Troubleshooting
 export ECS_API=http://0.0.0.0:8000
@@ -18,7 +8,5 @@ export AGENT_POST_API=https://postman-echo.com
 echo "Checking ECS API Val"
 echo $ECS_API
 
-# move back a folder for catkin_make
-cd ..
-# catkin_make
-# catkin_make run_tests
+
+python3 rosrect-ecs-api-server/src/ecs_endpoint.py
