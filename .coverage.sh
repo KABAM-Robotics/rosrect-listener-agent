4604718d925061b82a09c7bcf1faf00321693289
Alok Pathak <alok@cognicept.systems>
2020-09-25 18:39:33 +0800

    Changed coverage sh file

--- .coverage.sh
+++ .coverage.sh
@@ -2,9 +2,12 @@
 #!/bin/bash
 
 ls
-sudo chmod +x /root/target_ws
-sudo cd /root/target_ws
-gcovr -r . --exclude-directories test
+WS="/root/target_ws"
+
+if [[ ! -z $(find $WS -type f -name '*.gcda') ]]; then
+    gcovr -r $WS --exclude-directories test --xml-pretty > coverage_cpp.xml
+fi
+#gcovr -r . --exclude-directories test
 
 # Actually upload coverage information
 #bash <(curl -s https://codecov.io/bash) -s "$ws/build/robot_calibration/"
