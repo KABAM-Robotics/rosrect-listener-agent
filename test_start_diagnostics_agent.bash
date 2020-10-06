eb7ec935c43f79e9c1ceb1317489c8b0c238e7bb
swaroophs <swarooph@cognicept.systems>
2020-10-06 09:48:35 +0800

    Changing roslaunch to rosrun for rosmaster reconnection.

--- test_start_diagnostics_agent.bash
+++ test_start_diagnostics_agent.bash
@@ -8,11 +8,11 @@ docker stop cgs_diagnostics_agent
 
 docker rm cgs_diagnostics_agent 
 
-docker run -it \
+docker run -d \
 --env-file ~/.cognicept/runtime.env \
 --restart unless-stopped \
 --network=host \
 --name=cgs_diagnostics_agent  \
 --volume="${HOME}/.cognicept/agent/logs:/root/.cognicept/agent/logs" \
 cognicept_diagnostics_agent:jeff  \
-roslaunch rosrect-listener-agent listener-agent.launch
+rosrun rosrect-listener-agent rosrect-listener-agent
