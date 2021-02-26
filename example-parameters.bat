989779c9180ef1087806d5a87362c20bb8ee4389
swaroophs <swaroophs@cognicept.systems>
2021-02-26 18:45:37 +0530

    Sample parameters files for Windows

---
+++ example-parameters.bat
@@ -0,0 +1,30 @@
+@REM Robot code
+set ROBOT_CODE=48db4392-c4dc-4ab8-a883-19b5606664d2
+
+@REM Site code
+set SITE_CODE=5864bf5c-9079-4eff-84e0-7c0827e06594
+
+@REM Agent type
+set AGENT_TYPE=ERT
+
+@REM Agent mode
+set AGENT_MODE=POST_TEST
+
+@REM Agent ID
+set AGENT_ID=bc548dcc-42b8-4f82-a2e4-8ffa8c0f794f
+
+@REM ECS API
+set ECS_API=http://localhost:8000
+
+@REM ECS ROBOT MODEL
+set ECS_ROBOT_MODEL=Turtlebot3
+
+@REM POST API
+set AGENT_POST_API=http://localhost:8042
+
+@REM ROS Config Examples
+@REM ROS_MASTER_URI=http://192.168.11.87:11311/
+@REM ROS_IP=192.168.11.87
+@REM LOG_NODE_LIST=/move_base;/amcl
+@REM LOG_NODE_EX_LIST=/rviz
+@REM DIAGNOSTICS=ON
